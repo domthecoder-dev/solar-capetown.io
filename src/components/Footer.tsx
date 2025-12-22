095dd5a7cbf615d5ae15099843b4d8ccd617ca25
domthecoder-dev <dominic3lliott@gmail.com>
2025-12-22 18:16:41 +0200

    ght
    delted - restart

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white py-10 text-center">
-      <p className="text-lg">
-        © {new Date().getFullYear()} GoSolarCPT.co.za – Cape Town’s #1 solar lead platform
-      </p>
-    </footer>
-  );
-}
